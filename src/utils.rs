71f2d7dd459590039a61f153c8c9ca74fede28de
kugo12 <szczepan7700@gmail.com>
2021-02-07 15:50:23 +0100

    Add length check

--- src/utils.rs
+++ src/utils.rs
@@ -6,6 +6,7 @@ pub mod rust {
             Some(data) => {
                 let a: Vec<_> = data.split(",").filter(|&x| x.starts_with("born")).map(|e| (e[4..]).parse::<i32>()).collect();
 
+                if a.len() == 0 { return None; }
                 if a[0].is_err() { return None };
                 
                 Some(a[0].clone().unwrap())
